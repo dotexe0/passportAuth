e5d8ec731d1a8c6d98d7446342c7ee0c902ea3de
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-12-12 20:47:43 -0800

    initial commit with working auth salt/hash

---
+++ index.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var User = require('./user-model');
+
+var app = express();
+
+var jsonParser = bodyParser.json();
+var bcrypt = require('bcryptjs');
+
+app.post('/users', jsonParser, function(req, res) {
+    // Body validation code
+    if (!req.body) {
+        return res.status(400).json({
+            message: "No request body"
+        });
+    }
+
+    if (!('username' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: username'
+        });
+    }
+
+    var username = req.body.username;
+
+    if (typeof username !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: username'
+        });
+    }
+
+    username = username.trim();
+
+    if (username === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: username'
+        });
+    }
+
+    if (!('password' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: password'
+        });
+    }
+
+    var password = req.body.password;
+
+    if (typeof password !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: password'
+        });
+    }
+
+    password = password.trim();
+
+    if (password === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: password'
+        });
+    }
+
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+
+        bcrypt.hash(password, salt, function(err, hash) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Internal server error'
+                });
+            }
+
+            var user = new User({
+                username: username,
+                password: hash
+            });
+
+            user.save(function(err) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Internal server error'
+                    });
+                }
+
+                return res.status(201).json({});
+            });
+        });
+    });
+});
+
+
+mongoose.connect('mongodb://localhost/auth').then(function() {
+    app.listen(process.env.PORT || 8080);
+});
